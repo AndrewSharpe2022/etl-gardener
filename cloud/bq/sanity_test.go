99bcb1b4d08a6e0598d6bcfdcf04753b93155a00
Greg Russell <gfr10598@gmail.com>
2018-10-12 14:22:22 -0400

    example using bqfake

--- cloud/bq/sanity_test.go
+++ cloud/bq/sanity_test.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"strings"
 	"testing"
+	"time"
 
+	"cloud.google.com/go/bigquery"
+	"github.com/m-lab/go/cloud/bqfake"
 	"github.com/m-lab/go/dataset"
 )
 
@@ -70,5 +73,53 @@ func TestSanityCheckAndCopy(t *testing.T) {
 	}
 	if !strings.HasPrefix(err.Error(), "googleapi: Error 404") {
 		t.Fatal(err)
+	}
+}
+
+func TestCachedMeta(t *testing.T) {
+	ctx := context.Background()
+	c, err := bqfake.NewClient(ctx, "mlab-testing")
+	if err != nil {
+		panic(err)
+	}
+	ds := c.Dataset("etl")
+
+	// TODO - also test table before it exists
+	// Test whether changes to table can be seen in existing table objects.
+
+	{
+		meta := bigquery.TableMetadata{CreationTime: time.Now(), LastModifiedTime: time.Now(), NumBytes: 168, NumRows: 8}
+		meta.TimePartitioning = &bigquery.TimePartitioning{Expiration: 0 * time.Second}
+		tbl := ds.Table("DedupTest")
+		err := tbl.Create(ctx, &meta)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	tbl := ds.Table("DedupTest")
+	log.Println(tbl)
+	meta, err := tbl.Metadata(ctx)
+	if err != nil {
+		t.Error(err)
+	} else if meta == nil {
+		t.Error("Meta should not be nil")
+	}
+
+	dsExt := dataset.Dataset{Dataset: ds, BqClient: *c}
+	at := NewAnnotatedTable(tbl, &dsExt)
+	// Fetch cache detail - which hits backend
+	meta, err = at.CachedMeta(ctx)
+	if err != nil {
+		t.Error(err)
+	} else if meta == nil {
+		t.Error("Meta should not be nil")
+	}
+	// Fetch again, exercising the cached code path.
+	meta, err = at.CachedMeta(ctx)
+	if err != nil {
+		t.Error(err)
+	} else if meta == nil {
+		t.Error("Meta should not be nil")
 	}
 }
